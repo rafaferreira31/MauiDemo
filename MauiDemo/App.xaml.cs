eabf70eb62c8d617b23972303fe95ee7a5e712b6
Rafael Ferreira <Rafael.Ferreira.35139@formandos.cinel.pt>
2024-12-10 20:06:26 +0000

    MauiDemo

--- MauiDemo/App.xaml.cs
+++ MauiDemo/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiDemo
+﻿using System.Net.WebSockets;
+
+namespace MauiDemo
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new AbsoluteLayoutDemo());
         }
     }
 }
---
+++ MauiDemo/TabedPageDemo.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiDemo;
+
+public partial class TabedPageDemo : TabbedPage
+{
+	public TabedPageDemo()
+	{
+		InitializeComponent();
+	}
+}
